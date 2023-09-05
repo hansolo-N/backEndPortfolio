042b33c4033beea834d01686c7f82121be9451f7
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-09-05 20:24:20 +0200

    mg2

--- email.js
+++ email.js
@@ -4,9 +4,8 @@ require("dotenv").config()
 const formData = require('form-data');
 const Mailgun = require('mailgun.js');
 const mailgun = new Mailgun(formData);
-const mg = mailgun.client({
-	username: process.env.USERNAME,
-	key: process.env.PRIVATE_KEY,
-});
+const mg = mailgun.client({username: process.env.USERNAME, key: process.env.MAILGUN_API_KEY});
+
+console.log(mg)
 
 module.exports = mg

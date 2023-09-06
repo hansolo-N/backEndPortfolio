2730aac914993d96f3b056de03533690986f7c93
hansolo-N <91372832+hansolo-N@users.noreply.github.com>
2023-09-06 09:22:23 +0200

    test

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ app.get('/',(req,res)=>{
 
 app.post('/email',async (req,res)=>{
     // res.header("Access-Control-Allow-Origin", "*");
-	console.log(req.body)
+	console.log(req.body.email)
     try {
 		await sendEmail.messages.create("sandbox77077025e4154927a411f47eac0eee56.mailgun.org", {
 			from: req.body.email,
